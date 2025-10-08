dfc8ddaa1d8ac1699894316ddf3f1f20082bec75
HP <mariayousif318@gmail.com>
2025-10-08 03:39:03 +0300

    list

--- lib/screens/facebook_screen.dart
+++ lib/screens/facebook_screen.dart
@@ -6,6 +6,17 @@ class FacebookScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: CustomFaceBookPost());
+    return  Scaffold(
+        body: SafeArea(
+            child:ListView(
+              children: [
+                CustomFaceBookPost(),
+                CustomFaceBookPost(),
+                CustomFaceBookPost(),
+                CustomFaceBookPost(),
+              ],
+            ),
+        ),
+    );
   }
 }
